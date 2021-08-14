f3fb722cd1e4489c99ba692347d245e5603b84bd
vitorfiler <evitorfiler@gmail.com>
2021-08-13 21:24:17 -0300

    url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   hmr       : false,
-  URL_API: "https://89199679ae19.ngrok.io",
+  URL_API: "https://66459a1470fb.ngrok.io",
   URL_AWS: "",
   ambiente: "prod",
 
