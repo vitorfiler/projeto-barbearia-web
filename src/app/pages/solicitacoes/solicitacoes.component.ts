2e3556f38d19cb5a721c7f2d8967c404e151e348
Junio <ejunioms@gmail.com>
2022-01-24 22:33:53 -0300

    select do status

--- src/app/pages/solicitacoes/solicitacoes.component.ts
+++ src/app/pages/solicitacoes/solicitacoes.component.ts
@@ -3,30 +3,41 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { SolicitacoesService } from './../../services/solicitacoes.service';
 import { Solicitacoes } from 'src/app/_models/solicitacoes';
-import { MatSort } from '@angular/material/sort';
+
 @Component({
   selector: 'vex-solicitacoes',
   templateUrl: './solicitacoes.component.html',
   styleUrls: ['./solicitacoes.component.scss'],
 })
 export class SolicitacoesComponent implements OnInit {
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+
+
 
   displayedColumns: string[] = ['clienteID', 'nomeServico', 'tempoEstimado', 'valor', 'dtAtendimento', 'responsavel', 'status'];
   dataSource = new MatTableDataSource<Solicitacoes>();
 
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+
 
   solicitacoes: Solicitacoes[] = [];
+
+  status: any[] = [
+    { value: 'aceito-0', viewValue: 'Aceito' },
+    { value: 'recusado-1', viewValue: 'Recusado' },
+    { value: 'pendente-2', viewValue: 'Pendente' },
+  ];
+
   constructor(private solicitacoesService: SolicitacoesService) {
   }
 
   ngOnInit(): void {
     this.getSolicitacoes();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   getSolicitacoes() {
@@ -36,6 +47,7 @@ export class SolicitacoesComponent implements OnInit {
         this.solicitacoes = response.body;
         console.log(this.solicitacoes[0].cliente);
         this.dataSource = new MatTableDataSource<Solicitacoes>(this.solicitacoes);
+        // setTimeout(() => this.paginator = new MatPaginator<>());
       }
 
     })
@@ -46,7 +58,3 @@ export class SolicitacoesComponent implements OnInit {
   }
 
 }
-
-
-
-
