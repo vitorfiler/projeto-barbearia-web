5bd75d13411c7676057809e0ac845633430b8215
Vitor Nunes <vnmacedo@stefanini.com>
2022-02-03 10:55:19 -0300

    ajuste interceptor

--- src/app/_config/token-Interceptor.service.ts
+++ src/app/_config/token-Interceptor.service.ts
@@ -1,13 +1,15 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { CommomService } from "../services/commom.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
 
-    constructor() { }
+    constructor(private commomService: CommomService) { }
 
     intercept(req: HttpRequest<any>,
         next: HttpHandler): Observable<HttpEvent<any>> {
@@ -22,7 +24,14 @@ export class TokenInterceptorService implements HttpInterceptor {
             );
             cloned.headers.set("Content-Type", "application/json");
 
-            return next.handle(cloned);
+            return next.handle(cloned).pipe(catchError(err => {
+                if (err.status === 401 || err.status === 403) {
+                    this.commomService.logout();
+                    setTimeout(() => {alert("Sessão Expirada!")}, 200);
+                }
+                const error = err.error.message || err.statusText;
+                    return throwError(error);
+            }));
         }
         else {
             return next.handle(req);
