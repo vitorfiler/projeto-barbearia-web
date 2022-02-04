c0b1e5d028ea7813b2f984854f9af87bfabd11a1
Vitor Nunes <vnmacedo@stefanini.com>
2022-02-04 14:54:07 -0300

    port

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ app.get('/',function(req,res){
     res.sendFile('index.html', {root: 'dist/vex/'}
   );
 })
-app.listen(port)
+app.listen(port, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
