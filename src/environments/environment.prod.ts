7d20e62b5017e0e2b63a44236499f3355d73886b
Vitor Nunes <vnmacedo@stefanini.com>
2022-02-04 20:47:01 -0300

    ajuste deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr       : false,
-  URL_API: "https://easycut-api.herokuapp.com",
+  URL_API: "http://e0f9-2804-389-c04f-4207-e5a7-eee1-da4f-c1ef.ngrok.io",
   URL_AWS: "",
   ambiente: "prd",
 
