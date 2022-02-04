ebe110ea0faac68dde8f430c2d4bef6d8b7e41f4
Vitor Nunes <vnmacedo@stefanini.com>
2022-02-04 11:59:15 -0300

    WIP: heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   hmr       : false,
   URL_API: "http://localhost:8080",
   URL_AWS: "",
