3c6f7e04d88492dc2bfc7342ae2e9f33cf9a2ce9
Vitor Nunes <vnmacedo@stefanini.com>
2022-02-05 02:38:47 -0300

    dfsdf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr       : false,
-  URL_API: "http://e0f9-2804-389-c04f-4207-e5a7-eee1-da4f-c1ef.ngrok.io",
+  URL_API: "https://7a4a-2804-14d-ba83-5697-f403-a713-144b-dbab.ngrok.io",
   URL_AWS: "",
   ambiente: "prd",
 
