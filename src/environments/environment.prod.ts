cba53af912df9933f089b4600157962ea0072d6b
Vitor Nunes <vnmacedo@stefanini.com>
2022-02-05 03:51:22 -0300

    ajuste url produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr       : false,
-  URL_API: "https://7a4a-2804-14d-ba83-5697-f403-a713-144b-dbab.ngrok.io",
+  URL_API: "https://easycut-api.herokuapp.com",
   URL_AWS: "",
   ambiente: "prd",
 
