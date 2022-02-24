da29353ad218240534a41beeecc0ccef72bc7f68
sarah silva <sarah.silvaon@gmail.com>
2022-02-23 21:40:49 -0300

    Criação de filtro e select inicial
    Commit realizado, nenhuma função efetivamente completa

--- src/app/pages/servicos/listagem-servicos/listagem-servicos.component.ts
+++ src/app/pages/servicos/listagem-servicos/listagem-servicos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Categoria } from 'src/app/_models/categoria';
+import { ModalAdicionarServico } from '../../modais/servicoes-modais/modal-adicionar-servicos';
 
 @Component({
   selector: 'vex-listagem-servicos',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listagem-servicos.component.scss']
 })
 export class ListagemServicosComponent implements OnInit {
+  form: FormGroup;
+  selecaoCategoria: Categoria[] = [
+    { value: 'TODOS', viewValue: 'Todos' },
+    { value: 'CABELOF', viewValue: 'Cabelo Feminino' },
+    { value: 'CABELOM', viewValue: 'Cabelo Masculino' },
+    { value: 'UNHAEPELE', viewValue: 'Unha e pele' },
+  ];
+  constructor(private router: Router,
+    private fb: FormBuilder,
+    public dialog: MatDialog) {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  inicializarFiltro() {
+    this.form = this.fb.group({
+      categoria: [this.selecaoCategoria[0].value]
+    });
+  }
+
+  ngOnInit(): void {
+    this.inicializarFiltro();
+  }
+
+  abrirModalAdicionarServico(isAdicionar: boolean) {
+    let dialogRef;
+
+    if (isAdicionar) {
+      dialogRef = this.dialog.open(ModalAdicionarServico)
+    }
+  }
 }
