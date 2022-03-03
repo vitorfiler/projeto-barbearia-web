aaacc8115fea6232422f2056a0d1519befe3cc1a
sarah silva <sarah.silvaon@gmail.com>
2022-03-03 00:35:53 -0300

    Formatações nas tabelas

---
+++ src/app/value-time.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ValueTimePipe } from './value-time.pipe';
+
+describe('ValueTimePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ValueTimePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
