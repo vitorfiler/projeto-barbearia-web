fd9ea4e4eb4509d3d43638b64112fd27a194e82a
Junio <ejunioms@gmail.com>
2022-03-17 19:11:41 -0300

    Inclusão gráfico vertical

--- src/app/pages/painel/grafico-de-barras-verticais/grafico-de-barras-verticais.component.ts
+++ src/app/pages/painel/grafico-de-barras-verticais/grafico-de-barras-verticais.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single } from './data';
 
 @Component({
   selector: 'vex-grafico-de-barras-verticais',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficoDeBarrasVerticaisComponent implements OnInit {
 
-  constructor() { }
+  single: any[];
+  multi: any[];
 
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { single })
+  }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  onSelect(event) {
+    console.log(event);
+  }
 }
--- src/app/pages/painel/painel.component.ts
+++ src/app/pages/painel/painel.component.ts
@@ -16,24 +16,6 @@ export class PainelComponent implements OnInit {
   graficoLinhas: boolean = false;
   graficoVerticalAgrupado: boolean = false;
 
-  constructor() { }
-
-  view: any[] = [700, 400];
-
-  // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Country';
-  showYAxisLabel = true;
-  yAxisLabel = 'Population';
-
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  };
-
   constructor() {
     Object.assign(this, { single })
   }
