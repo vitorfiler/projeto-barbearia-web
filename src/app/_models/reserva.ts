e8045ad991f869d6cbe3bdd2d92ef906e8e4e03d
Junio <ejunioms@gmail.com>
2022-03-20 20:17:59 -0300

    Revert "Merge branch 'dev' of https://github.com/vitorfiler/projeto-barbearia-web into PDV031-2"
    This reverts commit 62b39ec8cab2103b97bbf99c0e76cca6999225d2, reversing
    changes made to fa0fba1ad6e84989f37f0350a26f70434e8cd4ec.

--- src/app/_models/reserva.ts
+++ src/app/_models/reserva.ts
@@ -1,23 +1,11 @@
 
-import { Cliente } from "./cliente";
-import { Produto } from "./produto";
-
 export class Reserva {
-	id: number;
-	dtAbertura: Date;
-	dtFinalizacao: Date;
-	codVendedor: string;
-	codReserva: string;
-	statusReserva: string;
-	valorTotalReserva: number;
-	motivoCancelamento: string;
-	solicitaCancelamento: boolean;
-	nomeCliente: string;
-	produtos: Produto;
-	estabelecimentoID: number;
-	cliente: Cliente;
-
-	constructor(reserva: Reserva = {} as Reserva) {
-		Object.assign(this, reserva);
-	}
+  id: number;
+  cliente: string;
+  produto: string;
+  quantidade: string;
+  valor: number;
+  dataRetirada: Date;
+  status: string;
+  estabelecimentoID: number;
 }
