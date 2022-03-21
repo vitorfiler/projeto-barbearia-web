569f0061d33d5a3a47a6ba45b3d1e9ef6b9463d9
Ricardo Rodrigues de Oliveira Junior <49491955+ricardorojr@users.noreply.github.com>
2022-03-21 19:12:23 -0300

    Grafico de pizza

--- src/app/pages/painel/painel.component.ts
+++ src/app/pages/painel/painel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { single } from './data';
 
 @Component({
   selector: 'vex-painel',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    Object.assign(this, { single })
+  }
 
   ngOnInit(): void {
   }
 
+
+  
+  name = 'Angular';
+  single: any[];
+  multi: any[];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+
+  
 }
