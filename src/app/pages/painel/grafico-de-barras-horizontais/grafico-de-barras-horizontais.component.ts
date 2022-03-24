0583d3d726dc2799bddab0ad427f7d9fe84ba668
Haniery L. Trigilio <haniery01@gmail.com>
2022-03-24 20:48:44 -0300

    grafico-barras-vertical-agrupadas

--- src/app/pages/painel/grafico-de-barras-horizontais/grafico-de-barras-horizontais.component.ts
+++ src/app/pages/painel/grafico-de-barras-horizontais/grafico-de-barras-horizontais.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { GraficosService } from 'src/app/services/graficos.service';
+
 
 @Component({
-  selector: 'vex-grafico-de-barras-horizontais',
-  templateUrl: './grafico-de-barras-horizontais.component.html',
-  styleUrls: ['./grafico-de-barras-horizontais.component.scss']
+	selector: 'vex-grafico-de-barras-horizontais',
+	templateUrl: './grafico-de-barras-horizontais.component.html',
+	styleUrls: ['./grafico-de-barras-horizontais.component.scss']
 })
+
+
 export class GraficoDeBarrasHorizontaisComponent implements OnInit {
+	single = []
+	view: any[] = [700, 400];
 
-  constructor() { }
+	// options
+	showXAxis: boolean = true;
+	showYAxis: boolean = true;
+	gradient: boolean = false;
+	showLegend: boolean = true;
+	showXAxisLabel: boolean = true;
+	yAxisLabel: string = 'Serviços';
+	showYAxisLabel: boolean = true;
+	xAxisLabel: string = 'Quantidade/Dia';
+	showGridLines: boolean = false;
+	barPadding: number = 2;
+	colorScheme = {
+		domain: ['#006fe2', '#cadede', '#c8d5ff', '#214377', '#000000']
+	};
 
-  ngOnInit(): void {
-  }
+	constructor(private graficoService: GraficosService) {
+	}
 
+	ngOnInit(): void {
+		this.graficoService.dadosGrafico().subscribe(response => {
+			this.single = response.body
+			return this.single;
+		})
+	}
 }
